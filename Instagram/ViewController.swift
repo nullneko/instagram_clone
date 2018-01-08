8fc6f809f894acb80ebb2219340be475170aac7a
hiroshikitahara <nullneko@HiroshiKitahara-no-MacBook-Pro.local>
2018-01-08 12:00:48 +0900

    GoogleService-Info.plist add

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import ESTabBarController
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTab()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +21,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func setupTab() {
+        // 
+        let tabBarController: ESTabBarController! = ESTabBarController(tabIconNames: ["home", "camera", "setting"])
+        tabBarController.selectedColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        tabBarController.buttonsBackgroundColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        
+        // 作成したESTabBarControllerを親のViewControllerに追加する
+        addChildViewController(tabBarController)
+        view.addSubview(tabBarController.view)
+        tabBarController.view.frame = view.bounds
+        tabBarController.didMove(toParentViewController: self)
+        
+        // タブをタップした時に表示するViewControllerを指定
+        let homeViewController = storyboard?.instantiateViewController(withIdentifier: "Home")
+        let settingViewController = storyboard?.instantiateViewController(withIdentifier: "Setting")
+        
+        tabBarController.setView(homeViewController, at: 0)
+        tabBarController.setView(settingViewController, at: 2)
+        
+        // 真ん中のタブはボタンとして扱う
+        tabBarController.highlightButton(at: 1)
+        tabBarController.setAction({
+            // ボタンがタップされたらImageViewControllerをモーダルで表示する
+            let imageViewController = self.storyboard?.instantiateViewController(withIdentifier: "ImageSelect")
+            self.present(imageViewController!, animated: true, completion: nil)
+        }, at: 1)
+    }
 
 }
 
